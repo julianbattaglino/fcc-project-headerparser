f1e9c45ad693b1165b9ed4cab4663fcbbff4c0c9
Julian Battaglino <julianbattaglino@gmail.com>
2022-03-02 21:27:55 -0300

    Api Endpoint Header Parser Response

--- server.js
+++ server.js
@@ -25,6 +25,17 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// Api Endpoint Header Parser Response
+app.get("/api/whoami", function (req, res) {
+  res.json({
+  "ipaddress": req.connection.remoteAddress,
+  "language": req.headers.["accept-language"],
+  "software": request.Headers["User-Agent"]
+
+    });
+  });
+
+
 
 
 // listen for requests :)
